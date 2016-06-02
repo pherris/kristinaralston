fa1266f33c72448cb54fdfe6835cc825dfde3ae5
Andrew Ralston <aralston@gmail.com>
2016-06-01 21:28:35 -0500

    exclude some files from s3sync

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Copy static assets with one year expiry."
-aws s3 sync ./ s3://kristinaralston/ --cache-control max-age=31536000 --exclude '*.html'
+aws s3 sync ./ s3://kristinaralston/ --cache-control max-age=31536000 --exclude '*.html' --exclude '*.sh' --exclude '.git/*'
 
 echo "Copy HTML files with one day expiry."
-aws s3 sync ./ s3://kristinaralston/ --cache-control max-age=86400 --include '*.html'
+aws s3 sync ./ s3://kristinaralston/ --cache-control max-age=86400 --include '*.html' --exclude '*.sh' --exclude '.git/*'
 
 echo "Invalidate CloudFront cache."
 aws cloudfront create-invalidation --distribution-id E2V7HA8PCMELU5 --paths /
