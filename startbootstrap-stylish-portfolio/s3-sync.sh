082ef7d37b8f7f13cbc5f64fd85e804fcb9858c6
Andrew Ralston <andrew.ralston@aftercode.io>
2020-07-26 21:43:53 -0500

    be legit

--- startbootstrap-stylish-portfolio/s3-sync.sh
+++ startbootstrap-stylish-portfolio/s3-sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Copy static assets with one year expiry."
-aws s3 sync ./ s3://kristinaralston/ --cache-control max-age=31536000 --exclude '*.html' --exclude '*.sh' --exclude '.git/*' --exclude 'node_modules/*'  --exclude 'LICENSE' --exclude 'README.md'  --exclude 'gulpfile.js' --exclude 'package.json' --exclude 'package-lock.json'
+aws s3 sync ./ s3://ralston-kristina-web/ --cache-control max-age=31536000 --exclude '*.html' --exclude '*.sh' --exclude '.git/*' --exclude 'node_modules/*'  --exclude 'LICENSE' --exclude 'README.md'  --exclude 'gulpfile.js' --exclude 'package.json' --exclude 'package-lock.json'
 
 echo "Copy HTML files with one day expiry."
-aws s3 sync ./ s3://kristinaralston/ --cache-control max-age=86400 --include '*.html' --exclude '*.sh' --exclude '.git/*' --exclude 'node_modules/*'  --exclude 'LICENSE' --exclude 'README.md'  --exclude 'gulpfile.js' --exclude 'package.json' --exclude 'package-lock.json'
+aws s3 sync ./ s3://ralston-kristina-web/ --cache-control max-age=86400 --include '*.html' --exclude '*.sh' --exclude '.git/*' --exclude 'node_modules/*'  --exclude 'LICENSE' --exclude 'README.md'  --exclude 'gulpfile.js' --exclude 'package.json' --exclude 'package-lock.json'
 
 echo "Invalidate CloudFront cache."
-aws cloudfront create-invalidation --distribution-id E2V7HA8PCMELU5 --paths /
+aws cloudfront create-invalidation --distribution-id E34Z9BDDLMC63N --paths /
