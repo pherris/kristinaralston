78176447446b4f549f7948a6b9c8ef65bb429e74
Andrew Ralston <andrew.ralston@aftercode.io>
2020-07-26 21:46:10 -0500

    update cf distro

--- startbootstrap-stylish-portfolio/s3-sync.sh
+++ startbootstrap-stylish-portfolio/s3-sync.sh
@@ -7,4 +7,4 @@ echo "Copy HTML files with one day expiry."
 aws s3 sync ./ s3://ralston-kristina-web/ --cache-control max-age=86400 --include '*.html' --exclude '*.sh' --exclude '.git/*' --exclude 'node_modules/*'  --exclude 'LICENSE' --exclude 'README.md'  --exclude 'gulpfile.js' --exclude 'package.json' --exclude 'package-lock.json'
 
 echo "Invalidate CloudFront cache."
-aws cloudfront create-invalidation --distribution-id E34Z9BDDLMC63N --paths /
+aws cloudfront create-invalidation --distribution-id E1DTJYXPXKA3KQ --paths /
